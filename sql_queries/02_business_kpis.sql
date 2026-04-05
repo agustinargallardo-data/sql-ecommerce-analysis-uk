1a917538d6db8451f3c5bee6b2afa848c75e1b07
Agustina Gallardo <agustinargallardo@gmail.com>
2026-04-05 20:53:37 -0300

    Update 02_business_kpis.sql

--- sql_queries/02_business_kpis.sql
+++ sql_queries/02_business_kpis.sql
@@ -1,1 +1,7 @@
-
+SELECT 
+    COUNT(*) AS total_ventas,
+    ROUND(SUM(line_total), 2) AS ingresos_totales,
+    COUNT(DISTINCT CustomerID) AS total_clientes,
+    ROUND(AVG(line_total), 2) AS ticket_promedio
+FROM 
+    `sql-ecommerce-analysis-uk.ecommerce_data.cleaned_sales`;
