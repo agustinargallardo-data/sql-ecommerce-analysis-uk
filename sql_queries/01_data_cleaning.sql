c855c06686b3b085c24e902e6a4fc6ce7feb2f12
Agustina Gallardo <agustinargallardo@gmail.com>
2026-04-05 20:51:22 -0300

    Update 01_data_cleaning.sql

--- sql_queries/01_data_cleaning.sql
+++ sql_queries/01_data_cleaning.sql
@@ -3,16 +3,11 @@ SELECT
     InvoiceNo,
     StockCode,
     Description,
-    -- Aseguramos que la cantidad sea un número entero
     CAST(Quantity AS INT64) AS Quantity,
-    -- Convertimos la fecha a formato DATETIME para análisis temporal
     CAST(InvoiceDate AS DATETIME) AS InvoiceDate, 
-    -- El precio unitario debe ser un número decimal (float)
     CAST(UnitPrice AS FLOAT64) AS UnitPrice,
-    -- El ID de cliente lo tratamos como STRING para evitar cálculos matemáticos con él
     CAST(CustomerID AS STRING) AS CustomerID, 
     Country,
-    -- Creamos la métrica de negocio: Total de la línea de venta
     (Quantity * UnitPrice) AS line_total 
 FROM 
     `sql-ecommerce-analysis-uk.ecommerce_data.raw_sales`
